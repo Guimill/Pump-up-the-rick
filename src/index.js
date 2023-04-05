3e441f13cc87a0c3146fed098388e617983dfcac
Guimill <guillaume.milleret@gmail.com>
2023-04-05 23:47:14 +0200

    some shit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import pumpup from './assets/video/pump.mp4'
+import './assets/style/css/style.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <main style={{display:"flex", justifyContent: "center", alignItems: "center", width:"100vw", height: "100vh", backgroundColor: "silver"}}>
+                <video style={{ width:"50vw", height: "50vh"}} controls>
+                    <source src={pumpup}/>
+                </video>
+        </main>
+    </React.StrictMode>
+);
